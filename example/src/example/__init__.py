13c8649141893cb7a0262fbb018d97543c435803
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-05-25 12:23:52 +0200

    structure for examples

---
+++ example/src/example/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/local/bin/python3
+"""Example module for the tableio-cfg-json bridge."""
+
+# Copyright (c) 2026 Tom Björkholm
+# MIT License
---
+++ example/test/test_example/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/local/bin/python3
+"""Tests for the example module."""
+
+# Copyright (c) 2026 Tom Björkholm
+# MIT License
