5f833294f28855986881d5b7be373e7a682b8449
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-06-21 13:42:41 +0200

    bump version number for new development

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup  # type: ignore[import-untyped]
 
 setup(
-  name='tableio-cfg-json', version='0.5',
+  name='tableio-cfg-json', version='0.5.1',
   description='config-as-json configuration encapsulation for tableio.',
   author='Tom Björkholm', author_email='klausuler_linnet0q@icloud.com',
   python_requires='>=3.12', packages=['tableio_cfg_json'],
